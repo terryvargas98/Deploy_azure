6591a3b561e6ac45671e7eec070a676abbf6795a
Terry Alberto Vargas Santa Cruz <terry.vargas@unas.edu.pe>
2020-10-07 14:16:53 -0500

    Agregar archivos de proyecto.

---
+++ Deploy_azure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Deploy_azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
